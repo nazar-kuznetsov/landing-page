4e1a025d2c5d316bf77b25477a15d91cbcab8c5d
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2017-08-13 12:21:15 +0300

    Slider-1.0

--- src/js/partials/oop-gallery.js
+++ src/js/partials/oop-gallery.js
@@ -1,191 +1,177 @@
-//  Следующий слайд
-Slider.prototype.dotsNext = function () {
-  this.dots[this.countDots].classList.remove('active');
+// Конструктор галерии
+function Slider(settings) {
+  this.btnPrev = document.querySelector(settings.prev);
+  this.btnNext = document.querySelector(settings.next);
 
-  this.countDots++;
+  this.selector = document.querySelector(settings.parent) || 
+                  document.querySelectorAll(settings.children);
 
-  if (this.countDots >= this.galleryItem.length) {
-      this.countDots = 0;
+  this.autoScroll = settings.auto || false;
+  this.autoScrollTime = settings.time || 7000;
+
+  this.count = 0;
+  this.countDots = 0;
+  
+  if (settings.dots !== undefined) {
+    this.dots = document.querySelectorAll(settings.dots);
+    this.galleryDots.call(this);
   }
+  
+  if (settings.scrollStyle === 'gallery') { this.gallery(); } 
+  if (settings.scrollStyle === 'carousel') { this.carousel(); }
 
-  this.dots[this.countDots].classList.add('active');
+}
+
+
+Slider.prototype.gallery = function () {
+  this.btnNext.addEventListener("click", this.nextGallery.bind(this));
+  this.btnPrev.addEventListener("click", this.prevGallery.bind(this));
+
+  // Автоматическое перелистывание
+  if (this.autoScroll) {
+    setInterval( () => {
+      this.nextGallery();
+    }, this.autoScrollTime);
+  }
+};
+
+
+Slider.prototype.carousel = function () {
+  this.btnNext.addEventListener("click", this.nextCarousel.bind(this));
+  this.btnPrev.addEventListener("click", this.prevCarousel.bind(this));
+
+
+  // Автоматическое перелистывание
+  if (this.autoScroll) {
+    setInterval( () => {
+      this.nextCarousel();
+    }, this.autoScrollTime);
+  }
 
 };
 
 
-Slider.prototype.dotsPrev = function () {
+Slider.prototype.nextGallery = function () {
 
-this.dots[this.countDots].classList.remove('active');
-
-
-  this.countDots--;
-
-  if (this.countDots < 0) {
-    this.countDots = this.galleryItem.length - 1;
-  }
-
-  this.dots[this.countDots].classList.add('active');
-
-
-};
-
-
-Slider.prototype.nextSlide = function () {
-
-  console.log(12);
-  if (this.dots !== undefined) {
-    this.dotsNext.call(this);
-  }
-
-  this.galleryItem[this.count].classList.remove('active');
-
-
-  this.count++;
-
-  if (this.count >= this.galleryItem.length) {
-      this.count = 0;
-  }
-
-  this.galleryItem[this.count].classList.add('active');
-
-
-};
-
-// // Преведущий слайд
-Slider.prototype.prevSlide = function () {
-
+    if (this.dots !== undefined) {
+      this.dotsNext.call(this);
+    }
+  
+    this.selector[this.count].classList.remove('active');
+    this.count++;
+  
+    if (this.count >= this.selector.length) {
+        this.count = 0;
+    }
+  
+    this.selector[this.count].classList.add('active');
+  
+  };
+  
+  // // Преведущий слайд
+Slider.prototype.prevGallery = function () {
+  
   if (this.dots !== undefined) {
     this.dotsPrev.call(this);
   }
 
-
-  this.galleryItem[this.count].classList.remove('active');
-
-
+  this.selector[this.count].classList.remove('active');
   this.count--;
 
   if (this.count < 0) {
-    this.count = this.galleryItem.length - 1;
+    this.count = this.selector.length - 1;
   }
 
-  this.galleryItem[this.count].classList.add('active');
- 
+  this.selector[this.count].classList.add('active');
 };
 
 
+
+Slider.prototype.nextCarousel = function () {
+  this.count++;
+
+  if (this.count >= this.selector.children.length) {
+    this.count = 0;
+  }
+
+  this.selector.style.transform = "translateX(-" + this.count + "00%)";
+};
+  
+  
+Slider.prototype.prevCarousel = function () {
+  this.count--;
+
+  if (this.count < 0) {
+    this.count = this.selector.children.length - 1;
+  }
+
+  this.selector.style.transform = "translateX(-" + this.count + "00%)";
+};
+
+
+
 Slider.prototype.galleryDots = function () {
+
   for ( let i = 0; i < this.dots.length; i++ ) {
     this.dots[i].addEventListener('click', this.getDotsNumber.bind(this));
   }
 }; 
 
 
-Slider.prototype.changeSlide = function () {
-
-  this.count = this.countDots;
-  
-  for ( let i = 0; i < this.galleryItem.length; i++ ) {
-    this.galleryItem[i].classList.remove('active');
-    this.dots[i].classList.remove('active');
-  }
-
-  this.galleryItem[this.count].classList.add('active');
-  this.dots[this.count].classList.add('active');
-
-};
-
-
 Slider.prototype.getDotsNumber = function (e) {
   this.dotsNumbers = e.target.getAttribute('data-gallery-dots');
   this.countDots = +this.dotsNumbers;
   this.changeSlide();
-  
 }; 
 
 
-
-
-Slider.prototype.nextCarusel = function () {
-  this.count++;
-
-  if (this.count >= this.galleryItem.children.length) {
-    this.count = 0;
-  }
-
-  this.galleryItem.style.transform = "translateX(-" + this.count + "00%)";
-
-};
-
-
-Slider.prototype.prevCarusel = function () {
-  this.count--;
-  
-    if (this.count < 0) {
-      this.count = this.galleryItem.children.length - 1;
-    }
-  
-    this.galleryItem.style.transform = "translateX(-" + this.count + "00%)";
-};
-
-
-
-
-// Конструктор галерии
-function Slider(obj) {
-  this.prev = document.querySelector(obj.prev);
-  this.next = document.querySelector(obj.next);
-  this.count = 0;
-  this.countDots = 0;
-  
-  // Значение по умолчание для autoSlider
-  if (obj.time === undefined) { obj.time = 8000; }
-  
-  
-  if (obj.dots !== undefined) {
-    this.dots = document.querySelectorAll(obj.dots);
-    this.galleryDots.call(this);
-  }
-  
-  
-  
-  // Автоматическое перелистывание
-  if (obj.auto) {
-    setInterval( () => {
-      this.nextSlide();
-    }, obj.time);
-  }
-  
-  
-  if (obj.sliderStyle === 'gallery') {
+Slider.prototype.changeSlide = function () {
+  this.count = this.countDots;
     
-    this.galleryItem = document.querySelectorAll(obj.gallery);
+  for ( let i = 0; i < this.selector.length; i++ ) {
+    this.selector[i].classList.remove('active');
+    this.dots[i].classList.remove('active');
+  }
+  
+  this.selector[this.count].classList.add('active');
+  this.dots[this.count].classList.add('active');
+};
 
-    this.next.addEventListener("click", this.nextSlide.bind(this));
-    this.prev.addEventListener("click", this.prevSlide.bind(this));
 
-  } else if (obj.sliderStyle === 'carusel') {
-    this.galleryItem = document.querySelector(obj.gallery);
+Slider.prototype.dotsNext = function () {
+  this.dots[this.countDots].classList.remove('active');
+  this.countDots++;
 
-    this.next.addEventListener("click", this.nextCarusel.bind(this));
-    this.prev.addEventListener("click", this.prevCarusel.bind(this));
-
+  if (this.countDots >= this.selector.length) {
+      this.countDots = 0;
   }
 
+  this.dots[this.countDots].classList.add('active');
+};
 
 
-}
+Slider.prototype.dotsPrev = function () {
+  this.dots[this.countDots].classList.remove('active');
+  this.countDots--;
+
+  if (this.countDots < 0) {
+    this.countDots = this.selector.length - 1;
+  }
+
+  this.dots[this.countDots].classList.add('active');
+};
+
+
+
 
 
 // Галерея Opacity
 let galleryTeam = new Slider ({
   prev: ".gallery__control [data-gallery=prev]",
   next: ".gallery__control [data-gallery=next]",
-  gallery: ".gallery__item",
-  sliderStyle: "gallery"
+  children: ".gallery__item",
+  scrollStyle: "gallery"
 });
-
-
-
 
 
 
@@ -194,14 +180,24 @@ let galleryTeam = new Slider ({
 let caruselTest = new Slider ({
   prev: ".carusel__control [data-carusel=prev]",
   next: ".carusel__control [data-carusel=next]",
-  gallery: ".carusel__list",
-  sliderStyle: "carusel"
+  parent: ".carusel__list",
+  scrollStyle: "carousel"
 });
 
 
 
+/*
+ 
+
+1.Доделать Dots
+3.Подумать где писать стили css
+4. Попробувать сделать свайпинг
+5.Авторотация всегда должна останавливаться при наведении курсора (42% так не делают)
+6.Авторотация должна полностью прекратиться после активного взаимодействия с пользователем
 
 
-// Доделать Dots
-// Доделать авто слайдер
-// Подумать где писать стили css
+
+
+*/
+
+
